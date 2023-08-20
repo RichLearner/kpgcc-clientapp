a40831fb4a61b8d8fa776c973feeee5c7d43d507
Aidan Yong <courselearner01@gmail.com>
2023-08-21 01:25:30 +0800

    uploaded and tested IIS

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Router, Route, Switch, Redirect  } from 'react-router-dom';
 import CaddyList from './components/CaddyList';
 import RatingForm from './components/RatingForm';
 import ThankYou from './components/ThankYou';
 
 function App() {
+    const weburl = "";
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/rate-caddies/:receipt" component={CaddyList} />
-                <Route path="/rating-form/:receipt/:chitId/:caddyId/:name" component={RatingForm} />
-                <Route path="/thankyou" component={ThankYou} />
+                <Route path={weburl+"rate-caddies/:receipt"} component={CaddyList} />
+                <Route path={weburl+"rating-form/:receipt/:chitId/:caddyId/:name"} component={RatingForm} />
+                <Route path={weburl+"thankyou"} component={ThankYou} />
+                {/* Default route */}
+                <Route path={weburl} component={CaddyList}/>
             </Switch>
         </BrowserRouter>
     );
